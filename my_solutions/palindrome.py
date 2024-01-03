e7d8427a8b207bf1b5a3ef3216b0ddff296a31bb
Angela Qin <134886388+AngelaQinn@users.noreply.github.com>
2024-01-03 22:05:29 +0000

    update

--- my_solutions/palindrome.py
+++ my_solutions/palindrome.py
@@ -1,6 +1,6 @@
 def is_palindrome(text):
   for i in range(0, int(len(text)/2)):
     if text[i] == text[len(text)-i-1]:
-      return True
+      print('yes')
     else:
-      return False
+      print('no')
